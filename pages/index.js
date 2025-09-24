287f78e9b58cc0fd91481c93e415c47c2f0e56a2
Eduarda Boaventura <eduarda.boaventura03@gmail.com>
2025-09-24 05:04:36 +0000

    correcao2 texto

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 function Home() {
-    return 
-    <h1>"Pronunciamento Oficial do Governo Bem Oficial Mesmo É de Verdade Eu Juro"</h1>;
-    <h2>"Lista de Pessoas que será presa em 2025 por:"</h2>;
-    <h3>"Cometer o crime de Burrice Culposa, quando não se há intenção de pensar"</h3>;
-    <h4>"1° - Bruno Eduardo Castro de Araújo, nascido em Uberlândia - MG"</h4>;
-    <p>"Fim da Lista"</p>
+    return (
+    <div>   
+       <h1>Pronunciamento Oficial do Governo Bem Oficial Mesmo É de Verdade Eu Juro</h1>
+       <h2>Lista de Pessoas que será presa em 2025 por:</h2>
+       <h3>Cometer o crime de Burrice Culposa, quando não se há intenção de pensar</h3>
+       <h4>1° - Bruno Eduardo Castro de Araújo, nascido em Uberlândia - MG</h4>
+       <p>Fim da Lista</p>
+    </div> )
 }   
 
 export default Home

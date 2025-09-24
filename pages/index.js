da18cf1c32330c49936176060026e618cf7caa6d
Eduarda Boaventura <eduarda.boaventura03@gmail.com>
2025-09-24 05:11:27 +0000

    atualizacao texto

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
        <h1>Pronunciamento Oficial do Governo Bem Oficial Mesmo É de Verdade Eu Juro</h1>
        <h2>Lista de Pessoas que será presa em 2025 por:</h2>
        <h3>Cometer o crime de Burrice Culposa, quando não se há intenção de pensar</h3>
-       <h4>1° - Bruno Eduardo Castro de Araújo, nascido em Uberlândia - MG</h4>
+       <h4>1° - Bruno Eduardo</h4>
        <p>Fim da Lista</p>
     </div> )
 }   
